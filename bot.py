8e73418d44dd1aefe6597bc12347c5a1607c3c57
Paolo Montelbano <paolomontelbano@gmail.com>
2024-02-20 13:27:07 -0500

    Message splitting, /close, custom logo
    Added message splitting to allow 2000+ character responses, /close command to close threads, and custom server logo

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import typing
 import os
+import asyncio
 from discord.ext import commands
 from discord import app_commands
 from config import discord_token, openai_token, custom_instructions, bot_name, admin_list
@@ -72,10 +73,10 @@ async def sync(interaction: discord.Interaction):
     if str(interaction.user) in admin_list:
         await bot.tree.sync()
         print("App Commands successfully synced to all guilds.")
-        await interaction.channel.send("App Commands successfully synced to all guilds.")
+        await interaction.response.send_message(content="App Commands successfully synced to all guilds.")
     else:
         print(f"{interaction.user} failed authorization check to sync guilds.")
-        await interaction.channel.send("You aren't authorized to use this command.")
+        await interaction.response.send_message(content="You aren't authorized to use this command.")
 
 
 @bot.event
@@ -124,26 +125,33 @@ async def on_voice_state_update(member: discord.Member, before, after):
 async def new_chat(interaction: discord.Interaction, prompt: typing.Optional[str]):
     global new_thread
     new_thread = True
-    if prompt: # If prompt is given
-        thread = await interaction.channel.create_thread(name=prompt, type=discord.ChannelType.public_thread)
-        message = await thread.send(interaction.user.mention)
-        response_message = await message.edit(content=f":hourglass:")
-        await thread.typing()
-        print(f"User {interaction.user} started a new chat with prompt: {prompt}")
-        await interaction.response.send_message(thread.jump_url)
+    if interaction.channel.type != discord.ChannelType.public_thread:
+        if prompt: # If prompt is given
+            thread = await interaction.channel.create_thread(name=prompt, type=discord.ChannelType.public_thread)
+            message = await thread.send(interaction.user.mention)
+            response_message = await message.edit(content=f":hourglass:")
+            await thread.typing()
+            print(f"User {interaction.user} started a new chat with prompt: {prompt}")
+            await interaction.response.send_message(thread.jump_url)
 
-        print("Received message from user. Sending API call...")
-        response = await getGPTResponse(message.channel, prompt)
-        try:
-            await response_message.edit(content=response)
-        except Exception as error:
-            await response_message.edit(content=error)
-    else: # If no prompt is given
-        thread = await interaction.channel.create_thread(name="New Thread", type=discord.ChannelType.public_thread)
-        message = await thread.send(interaction.user.mention)
-        waiting_message = await message.edit(content="Awaiting prompt...")
-        print(f"User {interaction.user} started a new chat with no prompt.")
-        await interaction.response.send_message(thread.jump_url)
+            print("Received message from user. Sending API call...")
+            response, withinCharLimit = await getGPTResponse(message.channel, prompt)
+            if withinCharLimit == True:
+                await response_message.edit(content=response)
+            else:
+                await response_message.edit(content=response[0])
+                for i in response[1:]:
+                    await message.channel.send(content=i)
+        else: # If no prompt is given
+            thread = await interaction.channel.create_thread(name="New Thread", type=discord.ChannelType.public_thread)
+            message = await thread.send(interaction.user.mention)
+            waiting_message = await message.edit(content="Awaiting prompt...")
+            print(f"User {interaction.user} started a new chat with no prompt.")
+            await interaction.response.send_message(thread.jump_url)
+    else:
+        await interaction.response.edit_message(content="New chats cannot be started in threads.")
+        asyncio.sleep(5)
+        await interaction.response.send_message
 
 # Responds to message in public threads
 @bot.event
@@ -153,17 +161,23 @@ async def on_message(message: discord.Message):
             print("Received message from user. Sending API call...")
             response_message = await message.channel.send(content=f":hourglass:")
             await message.channel.typing()
-            response = await getGPTResponse(message.channel, None)
-            await response_message.edit(content=response)
+            response, withinCharLimit = await getGPTResponse(message.channel, None)
+            if withinCharLimit == True: # Checks if the message fits within content limit
+                await response_message.edit(content=response)
+            else:
+                await response_message.edit(content=response[0])
+                for i in response[1:]:
+                    await message.channel.send(content=i)
+
             
 
 
 async def getGPTResponse(thread: discord.Thread, prompt: typing.Optional[str]):
     global chat_completion
-    messages_list = []
+    message_history_list = []
     
     if prompt: # Checks if there is a prompt passed to append before iterating through the chat history.
-        messages_list.append(
+        message_history_list.append(
             {
                 "role": "user",
                 "content": prompt
@@ -175,7 +189,7 @@ async def getGPTResponse(thread: discord.Thread, prompt: typing.Optional[str]):
             print(f"Initial prompt passed from user: {prompt}")
     async for message in thread.history(limit=None, oldest_first=True): # Grabs all messages in thread by iterating through chat history
         if not message.author.bot: # If user message
-            messages_list.append(
+            message_history_list.append(
                 {
                     "role": "user", # User is sender of message
                     "content": message.content # Content of user's message
@@ -183,7 +197,7 @@ async def getGPTResponse(thread: discord.Thread, prompt: typing.Optional[str]):
             )
         else: # If bot message
             if message.content != ":hourglass:" and message.content != "Awaiting prompt...": # Ensures it's not a placeholer message being passed
-                messages_list.append(
+                message_history_list.append(
                     {
                     "role": "assistant", # Bot is sender of message
                     "content": message.content # Content of bot's message
@@ -198,17 +212,23 @@ async def getGPTResponse(thread: discord.Thread, prompt: typing.Optional[str]):
             "role": "system",
             "content": custom_instructions,
         }
-    ] + messages_list,
+    ] + message_history_list,
     model="gpt-4-turbo-preview",
     )
 
     output = (chat_completion.choices[0].message.content)
-    output = formatGPTResponse(output)
-    print("OpenAI API call received.")
-    return output
+    output, withinCharLimit = await formatGPTResponse(output)
+    print("OpenAI response received.")
+    if withinCharLimit == False:
+        print(f"Output outside of character limit, consists of {len(output)} messages.")
+    else:
+        print(f"Output within character limit.")
+    return output, withinCharLimit
 
 async def formatGPTResponse(content):
-    if content > 2000: # Checks if content is within character limit
+    if len(content) <= 2000: # Checks if content is within character limit (2,000 or fewer)
+        return content, True # Returns string, True = fits within character limit
+    else:
         messages = []
         while len(content) > 0:
             # Split the content into a message of max_length or less
@@ -217,9 +237,7 @@ async def formatGPTResponse(content):
             content = content[2000:]
             # Append the message part to the messages list
             messages.append(message_part)
-        return messages # Returns array of messages to be sent
-    else:
-        return content # Returns content
+        return messages, False # Returns array of messages to be sent, False = doesn't fit within character limit
 
 
 class ClearConfirm(discord.ui.View):
@@ -324,8 +342,13 @@ async def clear(interaction: discord.Interaction):
                 print('Cancelling clear command...')
 
 @bot.tree.command(name='close', description="Closes the current thread, archiving but not deleting.")
-async def clear(interaction: discord.Interaction):
-    interaction.response.send_message("This currently doesn't do anything and is yet to be coded.")
+async def close(interaction: discord.Interaction):
+    if interaction.channel.type == discord.ChannelType.public_thread:
+        await interaction.response.send_message(content=":white_check_mark: Closed!")
+        await interaction.channel.edit(archived=True)
+    else:
+        await interaction.response.send_message(content="This is not a public thread, and so it can't be closed.")
+    # interaction.response.send_message("This currently doesn't do anything and is yet to be coded.")
     return
 
 @bot.tree.command(name="log", description="Saves a log of the thread as your choice of file.")
